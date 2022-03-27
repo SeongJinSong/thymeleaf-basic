0d6e35d23fdb51fe1f09d6cfaf6e1f094e402591
seongjin_song <seongjin_song@tmax.co.kr>
2022-03-27 10:33:07 +0900

    반복

--- src/main/java/spring/thymeleaf/basic/BasicController.java
+++ src/main/java/spring/thymeleaf/basic/BasicController.java
@@ -95,6 +95,13 @@ public class BasicController {
     public String attribute(){
         return "basic/attribute";
     }
+    
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+
+        return "basic/each";
+    }
 
 
     @Data
@@ -106,5 +113,14 @@ public class BasicController {
             this.username = username;
             this.age = age;
         }
+    }
+    
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
     }
 }
